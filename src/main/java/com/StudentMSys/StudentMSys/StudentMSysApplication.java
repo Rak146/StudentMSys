d4eb26a250ba4f295b743de50883b25055772312
Rakesh Babu Mudragada <mudragadarakesh333@gmail.com>
2025-09-08 15:09:50 -0400

    Initial commit -StudentMSys Project

---
+++ src/main/java/com/StudentMSys/StudentMSys/StudentMSysApplication.java
@@ -0,0 +1,13 @@
+package com.StudentMSys.StudentMSys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentMSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentMSysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/StudentMSys/StudentMSys/StudentMSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StudentMSys.StudentMSys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentMSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
